bc51f4bfc7564790d87ed498948b4194df9af606
Chilllyy <117584956+Chilllyy@users.noreply.github.com>
2025-04-09 15:11:09 -0600

    moved version file check to update function

--- update.lua
+++ update.lua
@@ -4,13 +4,12 @@ local user = "Chilllyy"
 local repo = "ComputerCraftFarmers"
 local branch = "main"
 
-local version_file = fs.open("/ver", 'r')
-local version = version_file.readAll()
-version_file.close()
-
 local url_template = "https://api.github.com/repos/" .. user .. "/" .. repo .. "/"
 
 function check()
+    local version_file = fs.open("/ver", 'r')
+    local version = version_file.readAll()
+    version_file.close()
     local url = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/" .. branch .. "/ver"
     local cloud = tonumber(git.getWebRaw(url))
     local current = tonumber(version)
